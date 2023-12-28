d5289011d6e9a448cf4284e84bae5e6a5f722433
Dexter <dexterautonomy@yahoo.com>
2023-12-28 02:48:00 +0100

    Initial commit

---
+++ src/main/java/com/hingebridge/devops/AuthorizationManagementResourceServerApplication.java
@@ -0,0 +1,11 @@
+package com.hingebridge.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationManagementResourceServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationManagementResourceServerApplication.class, args);
+	}
+}
