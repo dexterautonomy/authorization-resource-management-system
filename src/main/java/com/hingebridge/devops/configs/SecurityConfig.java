dffd9835f2f7c9264d7fd7ae68e7d3601527cbc8
Dexter <dexterautonomy@yahoo.com>
2024-03-02 15:53:30 +0100

    Got rid of the SCOPE_ and ROLE_ prefixes

--- src/main/java/com/hingebridge/devops/configs/SecurityConfig.java
+++ src/main/java/com/hingebridge/devops/configs/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> {
                             jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri));
-                            jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter()); //So we can use ROLE instead of SCOPE
+                            jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter()); //So we can remove ROLE_ as well as SCOPE_ from authorities
                         })
                 )
                 .build();
